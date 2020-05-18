6bd7520613abd7975dfb3c1a58726b150c786718
dudamaciej88 <dudamaciej88@gmail.com>
2020-05-18 23:22:07 +0200

    update

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 import * as S from "./button.style";
 
-const Button = () => {
+const Button = ({ children }) => {
+  const [opacityValue, setOpacityValue] = useState(0);
 
-    const [opacityValue, setOpacityValue] = useState(0)
-
-    useEffect(() => {
-        console.log("UseEffect")
-        setTimeout(() => {
-            setOpacityValue("1");
-          }, 1000);
-    }, [])
+  useEffect(() => {
+    console.log("UseEffect");
+    setTimeout(() => {
+      setOpacityValue("1");
+    }, 4000);
+  }, []);
   console.log("Button");
-  return <S.Button onClick={() => setOpacityValue("0")} opacityValue={opacityValue}>wybierz język</S.Button>;
+  return (
+    <S.Button onClick={() => setOpacityValue("0")} opacityValue={opacityValue}>
+      {children}
+    </S.Button>
+  );
 };
 
 export default Button;
